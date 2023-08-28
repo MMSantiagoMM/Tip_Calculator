cddf6215b45875988186a49a25bb92d5315b323a
Santiago Mosquera Mapura <s.mosquera@globant.com>
2023-08-27 21:06:29 -0500

    I finished this practice for now

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.tiptime
 import  androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.calculate.setOnClickListener {
+            calculateCheck()
+        }
+
+
+    }
+    private fun calculateCheck(){
+        val cuenta = binding.totalCost.text.toString()
+        val costTotal = cuenta.toDoubleOrNull()
+        if(costTotal==null){
+            binding.tipResult.text = ""
+            return
+        }
+        val radioGroup = binding.optionsTip.checkedRadioButtonId
+        val switch = binding.roundUpTip.isChecked
+        val percent = when(radioGroup){
+            R.id.twentyPercent -> 0.20
+            R.id.eighteenPercent -> 0.18
+            else -> 0.15
+        }
+        var tip = costTotal * percent
+        if(switch){
+            tip = ceil(tip)
+        }
+
+        var formmater = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tipTotal,formmater)
 
     }
 
 }
+
